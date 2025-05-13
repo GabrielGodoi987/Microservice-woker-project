2f5b940b35433838bdbf192d866b8a2631e55882
gabrielgodoi987 <gabrielgodoi452@gmail.com>
2025-05-12 23:47:27 -0300

    setting up load ballancer to manage instances of services

--- microservice/src/main/java/worker/gabrielgodoi/microservice/controller/WorkerController.java
+++ microservice/src/main/java/worker/gabrielgodoi/microservice/controller/WorkerController.java
@@ -1,6 +1,9 @@
 package worker.gabrielgodoi.microservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +18,9 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerController {
+    private static final Logger logger = LoggerFactory.getLogger(WorkerController.class);
     private final WorkerService workerService;
+    private final Environment env;
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
@@ -25,6 +30,7 @@ public class WorkerController {
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findOne(@PathVariable Long id) {
+        logger.info("PORT = " + this.env.getProperty("local.server.port"));
         Worker worker = this.workerService.findOne(id);
         return ResponseEntity.ok().body(worker);
     }
